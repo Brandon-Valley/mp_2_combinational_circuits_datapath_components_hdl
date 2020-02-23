35cdafa4d36110421b9c9848cfd13a2fc66b417b
Brandon Valley <bavvh8@mst.edu>
2020-02-23 10:53:05 -0600

    sim

--- vhdl/priority_enc_4_2.vhd
+++ vhdl/priority_enc_4_2.vhd
@@ -2,13 +2,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
---
+
+  -- (input  [3:0] i_code,
+   -- output [1:0] o_code,
+   -- output       o_valid);
 entity priority_enc_4_2 is
-  port ( i_a : in  std_logic;
-         i_b : in  std_logic;
-         i_c : in  std_logic;
-         i_d : in  std_logic;
-         o_f : out std_logic);
+  port ( 
+         i_code : in  std_logic_vector(3 downto 0);
+         o_code : out std_logic_vector(1 downto 0);
+         o_valid: out std_logic);
 end priority_enc_4_2;
 
 --------------------------------
@@ -17,46 +19,31 @@ end priority_enc_4_2;
 
 architecture equation of priority_enc_4_2 is
   begin
-    o_f <= (i_a or i_b or i_c or i_d);
+    -- o_f <= (i_a or i_b or i_c or i_d);
+    
+    o_code <= "01";
+    o_valid <= '1';
+    
+    
 end architecture equation;
 
 
---------------------------------
+-- ------------------------------
 -- Behavior Model
---------------------------------
-architecture behavior of priority_enc_4_2 is
-  begin
+-- ------------------------------
+-- architecture behavior of priority_enc_4_2 is
+  -- begin
 
            
-    o_f <= '1' when i_a = '1' else 
-           '1' when i_b = '1' else 
-           '1' when i_c = '1' else 
-           '1' when i_d = '1' else 
-           '0';
-end architecture behavior;
+    -- o_f <= '1' when i_a = '1' else 
+           -- '1' when i_b = '1' else 
+           -- '1' when i_c = '1' else 
+           -- '1' when i_d = '1' else 
+           -- '0';
+-- end architecture behavior;
 
 
 
---------------------------------
--- Component Model
---------------------------------
-architecture cmpnt of priority_enc_4_2 is
-  component OR2 is
-    port ( i_a : in  std_logic;
-           i_b : in  std_logic;
-           o_f : out std_logic);
-  end component OR2;
- 
-    
-  -- temp outs for OR2s
-  signal f_1_o : std_logic; 
-  signal f_2_o : std_logic; 
-    
-  begin
-    OR2_1 : OR2 port map (i_a,   i_b,   f_1_o);
-    OR2_2 : OR2 port map (i_c,   i_d,   f_2_o);
-    OR2_3 : OR2 port map (f_1_o, f_2_o, o_f);
-end architecture cmpnt;
     
     
 
--- vhdl/priority_enc_4_2_tb.vhd
+++ vhdl/priority_enc_4_2_tb.vhd
@@ -5,47 +5,73 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 
+  -- (input  [3:0] i_code,
+   -- output [1:0] o_code,
+   -- output       o_valid);
 entity priority_enc_4_2_tb is
 end entity priority_enc_4_2_tb;
 
 architecture verify of priority_enc_4_2_tb is
-  signal i_a   : std_logic;
-  signal i_b   : std_logic;
-  signal i_c   : std_logic;
-  signal i_d   : std_logic;
-  signal o_f   : std_logic;
+  -- signal i_a   : std_logic;
+  -- signal i_b   : std_logic;
+  -- signal i_c   : std_logic;
+  -- signal i_d   : std_logic;
+  -- signal o_f   : std_logic;
+  -- signal input : std_logic_vector (3 downto 0) := "0000";
+  
+  signal i_code : std_logic_vector(3 downto 0);
+  signal o_code : std_logic_vector(1 downto 0);
+  signal o_valid: std_logic;
+  
   signal input : std_logic_vector (3 downto 0) := "0000";
 
+
+  
+  
+
 begin
-  -- Model Options:  cmpnt, equation, behavior
-  duv: entity work.priority_enc_4_2(cmpnt)
-    port map( i_a => i_a,
-              i_b => i_b,
-              i_c => i_c,
-              i_d => i_d,
-              o_f => o_f);
+  duv: entity work.priority_enc_4_2(equation)
+    -- port map( i_a => i_a,
+              -- i_b => i_b,
+              -- i_c => i_c,
+              -- i_d => i_d,
+              -- o_f => o_f);
+    port map( 
+              i_code => i_code,
+              o_code => o_code,
+              o_valid => o_valid);
+
               
   apply_test_cases : process is
     procedure apply_test
       ( a_test, b_test, c_test, d_test : in std_logic) is
     begin 
-      i_a <= a_test;
-      i_b <= b_test;
-      i_c <= c_test;
-      i_d <= d_test;
+      -- i_a <= a_test;
+      -- i_b <= b_test;
+      -- i_c <= c_test;
+      -- i_d <= d_test;
+      
+      
+      
       wait for 1 ms;
     end procedure apply_test;
     
   begin
-    for i in 0 to 16 loop
+    for i in 0 to 18 loop
       apply_test(input(3), input(2), input(1), input(0));
+      
+      -- i_code <= input;
+      -- i_code <= "0010";
+      -- wait for 1 ms;
+      
+      
       input <= input + "0001";
      end loop;
   
   
   
     -- -- test
-    -- apply_test('0', '0', '0', '0');
+    apply_test('0', '0', '0', '0');
     -- apply_test('0', '1', '0', '0');
     -- apply_test('1', '1', '1', '1');
 
@@ -62,7 +88,45 @@ end architecture verify;
     
     
     
+-- begin
+  -- -- Model Options:  cmpnt, equation, behavior
+  -- duv: entity work.OR4(cmpnt)
+    -- port map( i_a => i_a,
+              -- i_b => i_b,
+              -- i_c => i_c,
+              -- i_d => i_d,
+              -- o_f => o_f);
+              
+  -- apply_test_cases : process is
+    -- procedure apply_test
+      -- ( a_test, b_test, c_test, d_test : in std_logic) is
+    -- begin 
+      -- i_a <= a_test;
+      -- i_b <= b_test;
+      -- i_c <= c_test;
+      -- i_d <= d_test;
+      -- wait for 1 ms;
+    -- end procedure apply_test;
     
+  -- begin
+    -- for i in 0 to 16 loop
+      -- apply_test(input(3), input(2), input(1), input(0));
+      -- input <= input + "0001";
+     -- end loop;
+  
+  
+  
+    -- test
+    -- -- apply_test('0', '0', '0', '0');
+    -- -- apply_test('0', '1', '0', '0');
+    -- -- apply_test('1', '1', '1', '1');
+
+
+    
+    -- wait;
+-- end process apply_test_cases;
+
+-- end architecture verify;
 
     
 
