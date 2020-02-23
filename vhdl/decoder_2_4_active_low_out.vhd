4f9477e3953a2458d247582a47d5a24c469be7d4
Brandon Valley <bavvh8@mst.edu>
2020-02-23 15:46:44 -0600

    VVV test sim works

--- vhdl/decoder_2_4_active_low_out.vhd
+++ vhdl/decoder_2_4_active_low_out.vhd
@@ -2,15 +2,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-
+-- sim:/decoder_2_4_active_low_out_tb/i_code \
+-- sim:/decoder_2_4_active_low_out_tb/0_code_n 
 entity decoder_2_4_active_low_out is
   port ( 
   
-        i_en   : in  std_logic;
-        i_code : in  std_logic_vector(3 downto 0);
-        o_A    : out std_logic;
-        o_L    : out std_logic;
-        o_B    : out std_logic);
+        i_code   : in  std_logic_vector(1 downto 0);
+        o_code_n : out std_logic_vector(3 downto 0));
 
 end decoder_2_4_active_low_out;
 
@@ -18,19 +16,21 @@ end decoder_2_4_active_low_out;
 architecture equation of decoder_2_4_active_low_out is
   begin
 
-    o_A <= 'Z' when i_en = '0'      else
-           '1' when i_code = "1010" or
-                    i_code = "1011" or
-                    i_code = "1100" else '0';
+    o_code_n <= "0010";
+    
+    -- o_A <= 'Z' when i_en = '0'      else
+           -- '1' when i_code = "1010" or
+                    -- i_code = "1011" or
+                    -- i_code = "1100" else '0';
        
-    o_L <= 'Z' when i_en = '0'      else
-           '1' when i_code = "0011" or
-                    i_code = "0100" or
-                    i_code = "0010" or
-                    i_code = "1110" else '0';
+    -- o_L <= 'Z' when i_en = '0'      else
+           -- '1' when i_code = "0011" or
+                    -- i_code = "0100" or
+                    -- i_code = "0010" or
+                    -- i_code = "1110" else '0';
                 
-    o_B <= 'Z' when i_en = '0'      else
-           '1' when i_code = "1111" else '0';                
+    -- o_B <= 'Z' when i_en = '0'      else
+           -- '1' when i_code = "1111" else '0';                
 
     
     
--- vhdl/decoder_2_4_active_low_out_tb.vhd
+++ vhdl/decoder_2_4_active_low_out_tb.vhd
@@ -11,55 +11,44 @@ end entity decoder_2_4_active_low_out_tb;
 architecture verify of decoder_2_4_active_low_out_tb is
 
 
-  signal i_en   : std_logic;
-  signal i_code : std_logic_vector(3 downto 0);
-  signal o_A    : std_logic;
-  signal o_L    : std_logic;
-  signal o_B    : std_logic;
+  signal i_code   : std_logic_vector(1 downto 0);
+  signal o_code_n : std_logic_vector(3 downto 0);
+
+  -- signal i_en   : std_logic;
+  -- signal i_code : std_logic_vector(3 downto 0);
+  -- signal o_A    : std_logic;
+  -- signal o_L    : std_logic;
+  -- signal o_B    : std_logic;
 
 
-  
-  -- signal i_en       : std_logic;        
-  -- signal i_code_0   : std_logic_vector(1 downto 0); 
-  -- signal i_code_1   : std_logic_vector(1 downto 0); 
-  -- signal i_code_2   : std_logic_vector(1 downto 0); 
-  -- signal i_code_3   : std_logic_vector(1 downto 0); 
-  -- signal i_sel_code : std_logic_vector(1 downto 0); 
-  -- signal o_code     : std_logic_vector(1 downto 0); 
-
-
-
-  signal input : std_logic_vector (4 downto 0) := "00000"; -- must be correct
+  signal input : std_logic_vector (1 downto 0) := "00"; -- must be correct
 
 begin
   duv: entity work.decoder_2_4_active_low_out(equation)
 
  
-  port map(i_en   => i_en  ,    
-           i_code => i_code,
-           o_A    => o_A   ,
-           o_L    => o_L   ,
-           o_B    => o_B     
+  port map(i_code   => i_code  ,   
+           o_code_n => o_code_n 
           ); 
               
               
   apply_test_cases : process is
     procedure apply_test
-      ( itst : in std_logic_vector(4 downto 0)) is
+      ( itst : in std_logic_vector(1 downto 0)) is
         
     begin 
         
-      i_code <= itst(3 downto 0);
-      i_en       <= itst(4);
+      i_code <= itst;
+      -- i_en       <= itst(4);
         
     
       wait for 1 ms;
     end procedure apply_test;
     
   begin
-    for i in 0 to 34 loop
+    for i in 0 to 6 loop
       apply_test(input);
-      input <= input + "0001";
+      input <= input + "01";
      end loop;    
     wait;
 end process apply_test_cases;
