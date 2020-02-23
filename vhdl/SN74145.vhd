30cbdcad065af5881dcd26da5d6dc4d5187b454b
Brandon Valley <bavvh8@mst.edu>
2020-02-23 12:03:32 -0600

    VVV test sim works

--- vhdl/SN74145.vhd
+++ vhdl/SN74145.vhd
@@ -3,35 +3,70 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 
-  -- (input  [3:0] i_code,
-   -- output [1:0] o_code,
-   -- output       o_valid);
 entity SN74145 is
   port ( 
-         i_code : in  std_logic_vector(3 downto 0);
-         o_code : out std_logic_vector(1 downto 0);
-         o_valid: out std_logic);
+         i_a : in std_logic;
+         i_b : in std_logic;
+         i_c : in std_logic;
+         i_d : in std_logic;
+         
+         i_cs     : in std_logic;
+         i_n_cs_0 : in std_logic;
+         i_n_cs_1 : in std_logic;
+         
+         o_0 : out std_logic;
+         o_1 : out std_logic;
+         o_2 : out std_logic;
+         o_3 : out std_logic;
+         o_4 : out std_logic;
+         o_5 : out std_logic;
+         o_6 : out std_logic;
+         o_7 : out std_logic;
+         o_8 : out std_logic;
+         o_9 : out std_logic);
+         
+         -- i_code : in  std_logic_vector(3 downto 0);
+         -- o_code : out std_logic_vector(1 downto 0);
+         -- o_valid: out std_logic);
+       
 end SN74145;
 
 
 
 architecture equation of SN74145 is
   begin
-    -- o_f <= (i_a or i_b or i_c or i_d);
+
+
+  o_0 <= '1';
+  o_1 <= '1';
+  o_2 <= '1';
+  o_3 <= '1';
+  o_4 <= '1';
+  o_5 <= '1';
+  o_6 <= '1';
+  o_7 <= '0';
+  o_8 <= '1';
+  o_9 <= '1';
+
+
+
+
+
+    -- -- o_f <= (i_a or i_b or i_c or i_d);
     
-    -- o_code <= "01";
-    -- o_valid <= '1';
+    -- -- o_code <= "01";
+    -- -- o_valid <= '1';
     
-    o_code <= "00" when i_code(0) = '1' else 
-              "01" when i_code(1) = '1' else 
-              "10" when i_code(2) = '1' else 
-              "11" when i_code(3) = '1' else 
-              "00";
+    -- o_code <= "00" when i_code(0) = '1' else 
+              -- "01" when i_code(1) = '1' else 
+              -- "10" when i_code(2) = '1' else 
+              -- "11" when i_code(3) = '1' else 
+              -- "00";
               
-    o_valid <= i_code(0) or 
-               i_code(1) or 
-               i_code(2) or 
-               i_code(3); 
+    -- o_valid <= i_code(0) or 
+               -- i_code(1) or 
+               -- i_code(2) or 
+               -- i_code(3); 
 
     
     
--- vhdl/SN74145_tb.vhd
+++ vhdl/SN74145_tb.vhd
@@ -12,49 +12,92 @@ entity SN74145_tb is
 end entity SN74145_tb;
 
 architecture verify of SN74145_tb is
-  -- signal i_a   : std_logic;
-  -- signal i_b   : std_logic;
-  -- signal i_c   : std_logic;
-  -- signal i_d   : std_logic;
-  -- signal o_f   : std_logic;
-  -- signal input : std_logic_vector (3 downto 0) := "0000";
+  signal i_a      : std_logic;
+  signal i_b      : std_logic;
+  signal i_c      : std_logic;
+  signal i_d      : std_logic;
   
-  signal i_code : std_logic_vector(3 downto 0);
-  signal o_code : std_logic_vector(1 downto 0);
-  signal o_valid: std_logic;
+  signal i_cs     : std_logic;
+  signal i_n_cs_0 : std_logic;
+  signal i_n_cs_1 : std_logic;
+
+  signal o_0      : std_logic;
+  signal o_1      : std_logic;
+  signal o_2      : std_logic;
+  signal o_3      : std_logic;
+  signal o_4      : std_logic;
+  signal o_5      : std_logic;
+  signal o_6      : std_logic;
+  signal o_7      : std_logic;
+  signal o_8      : std_logic;
+  signal o_9      : std_logic;
   
-  signal input : std_logic_vector (3 downto 0) := "0000";
+  -- signal i_code : std_logic_vector(3 downto 0);
+  -- signal o_code : std_logic_vector(1 downto 0);
+  -- signal o_valid: std_logic;
+  
+  signal input : std_logic_vector (6 downto 0) := "0000000";
 
 
   
-  
-
-begin
-  duv: entity work.SN74145(equation)
+    -- duv: entity work.OR4(cmpnt)
     -- port map( i_a => i_a,
               -- i_b => i_b,
               -- i_c => i_c,
               -- i_d => i_d,
               -- o_f => o_f);
-    port map( 
-              i_code => i_code,
-              o_code => o_code,
-              o_valid => o_valid);
 
+begin
+  duv: entity work.SN74145(equation)
+
+    port map( i_a      => i_a     ,  
+              
+              i_b      => i_b     ,  
+              i_c      => i_c     ,  
+              i_d      => i_d     ,  
+
+              i_cs     => i_cs    ,  
+              i_n_cs_0 => i_n_cs_0,  
+              i_n_cs_1 => i_n_cs_1,  
+
+              o_0      => o_0     ,  
+              o_1      => o_1     ,  
+              o_2      => o_2     ,  
+              o_3      => o_3     ,  
+              o_4      => o_4     ,  
+              o_5      => o_5     ,  
+              o_6      => o_6     ,  
+              o_7      => o_7     ,  
+              o_8      => o_8     ,
+              o_9      => o_9       
+              
+              ); 
               
   apply_test_cases : process is
     procedure apply_test
-      -- ( input_test : in std_logic_vector(3 downto 0) is
-      ( e0, e1, e2, e3 : in std_logic) is
+      ( itst : in std_logic_vector(6 downto 0)) is
+      -- ( e0, e1, e2, e3 : in std_logic) is                               -- 4
+      -- ( e0, e1, e2, e3, e4, e5, e6, e7 : in std_logic) is                  -- 7
+        
+       
+        
     begin 
-      -- i_a <= e0;
-      -- i_b <= e1;
-      -- i_c <= e2;
-      -- i_d <= e3;
-      i_code(0) <= e0;
-      i_code(1) <= e1;
-      i_code(2) <= e2;
-      i_code(3) <= e3;
+      
+      
+      i_a      <= itst(0);
+      i_b      <= itst(1);
+      i_c      <= itst(2);
+      i_d      <= itst(3);
+      i_cs     <= itst(4);
+      i_n_cs_0 <= itst(5);
+      i_n_cs_1 <= itst(6); 
+      
+      
+      
+      -- i_code(0) <= e0;
+      -- i_code(1) <= e1;
+      -- i_code(2) <= e2;
+      -- i_code(3) <= e3;
       
       
       wait for 1 ms;
@@ -62,8 +105,8 @@ begin
     
   begin
     for i in 0 to 18 loop
-      apply_test(input(3), input(2), input(1), input(0));
-      -- apply_test(input);
+      -- apply_test(input(3), input(2), input(1), input(0));
+      apply_test(input);
       
       -- i_code <= input;
       -- i_code <= "0010";
