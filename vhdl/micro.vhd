3c8cb291e76e9790c909dbe802e1edabd762900d
Brandon Valley <bavvh8@mst.edu>
2020-02-23 15:10:25 -0600

    VVV test sim works

--- vhdl/micro.vhd
+++ vhdl/micro.vhd
@@ -1,43 +1,66 @@
 -- python C:\Users\BrANDon\Documents\Personal_Projects\my_utils\modelsim_utils\auto_run.py -d run_cmd__micro.do
 library ieee;
 use ieee.std_logic_1164.all;
-
+  -- (
+  -- input        i_en,
+  -- input  [3:0] i_code,
+  -- output       o_A,
+  -- output       o_L,
+  -- output       o_B);
 
 entity micro is
   port ( 
-        i_en       : in  std_logic;        
-        i_code_0   : in  std_logic_vector(1 downto 0); 
-        i_code_1   : in  std_logic_vector(1 downto 0); 
-        i_code_2   : in  std_logic_vector(1 downto 0); 
-        i_code_3   : in  std_logic_vector(1 downto 0); 
-        i_sel_code : in  std_logic_vector(1 downto 0); 
-        o_code     : out std_logic_vector(1 downto 0)); 
+  
+        i_en   : in  std_logic;
+        i_code : in  std_logic_vector(3 downto 0);
+        o_A    : out std_logic;
+        o_L    : out std_logic;
+        o_B    : out std_logic);
+  
+        -- i_en       : in  std_logic;        
+        -- i_code_0   : in  std_logic_vector(1 downto 0); 
+        -- i_code_1   : in  std_logic_vector(1 downto 0); 
+        -- i_code_2   : in  std_logic_vector(1 downto 0); 
+        -- i_code_3   : in  std_logic_vector(1 downto 0); 
+        -- i_sel_code : in  std_logic_vector(1 downto 0); 
+        -- o_code     : out std_logic_vector(1 downto 0)); 
 
 end micro;
 
 
--- Component Model
+
+  -- assign o_A = ~i_en ? 'Z:
+               -- i_code == 4'b1010 | 
+               -- i_code == 4'b1011 |
+               -- i_code == 4'b1100 ? 1 : 0;
+  
+  -- assign o_L = ~i_en ? 'Z:
+               -- i_code == 4'b0011 | 
+               -- i_code == 4'b0100 |
+               -- i_code == 4'b0010 |
+               -- i_code == 4'b1110 ? 1 : 0;
+               
+  -- assign o_B = ~i_en ? 'Z:
+               -- i_code == 4'b1111 ? 1 : 0;
+
+
 architecture equation of micro is
-  component MUX_4_1 is
-  port ( 
-        i_en       : in  std_logic;
-        i_code     : in  std_logic_vector(3 downto 0);
-        i_sel_code : in  std_logic_vector(1 downto 0);
-        o_f        : out std_logic);
-  end component MUX_4_1;
-
-    -- -- temp outs
-  signal fi0 : std_logic_vector(3 downto 0) := i_code_0(0) & i_code_1(0) & i_code_2(0) & i_code_3(0);
-  signal fi1 : std_logic_vector(3 downto 0) := i_code_0(1) & i_code_1(1) & i_code_2(1) & i_code_3(1);
-
   begin
-    MUX1 : MUX_4_1 port map (i_en, fi0, i_sel_code, o_code(0));
-    MUX0 : MUX_4_1 port map (i_en, fi1, i_sel_code, o_code(1));
-   
+
+    o_A <= '1';
+    o_L <= '1';
+    o_B <= '1';
+
+  -- o_0 <= '0' when  i_a = '0' and i_b = '0' and i_c = '0' and i_d = '0' else '1';
+  -- o_1 <= '0' when  i_a = '0' and i_b = '0' and i_c = '0' and i_d = '1' else '1';
+  -- o_2 <= '0' when  i_a = '0' and i_b = '0' and i_c = '1' and i_d = '0' else '1';
+  -- o_3 <= '0' when  i_a = '0' and i_b = '0' and i_c = '1' and i_d = '1' else '1';
+  -- o_4 <= '0' when  i_a = '0' and i_b = '1' and i_c = '0' and i_d = '0' else '1';
+  -- o_5 <= '0' when  i_a = '0' and i_b = '1' and i_c = '0' and i_d = '1' else '1';
+  -- o_6 <= '0' when  i_a = '0' and i_b = '1' and i_c = '1' and i_d = '0' else '1';
+  -- o_7 <= '0' when  i_a = '0' and i_b = '1' and i_c = '1' and i_d = '1' else '1';
+  -- o_8 <= '0' when  i_a = '1' and i_b = '0' and i_c = '0' and i_d = '0' else '1';
+  -- o_9 <= '0' when  i_a = '1' and i_b = '0' and i_c = '0' and i_d = '1' else '1';
+    
+    
 end architecture equation;
-
-
-
-
-
-
--- vhdl/micro_tb.vhd
+++ vhdl/micro_tb.vhd
@@ -11,47 +11,46 @@ end entity micro_tb;
 architecture verify of micro_tb is
 
 
+  signal i_en   : std_logic;
+  signal i_code : std_logic_vector(3 downto 0);
+  signal o_A    : std_logic;
+  signal o_L    : std_logic;
+  signal o_B    : std_logic;
+
+
   
-  signal i_en       : std_logic;        
-  signal i_code_0   : std_logic_vector(1 downto 0); 
-  signal i_code_1   : std_logic_vector(1 downto 0); 
-  signal i_code_2   : std_logic_vector(1 downto 0); 
-  signal i_code_3   : std_logic_vector(1 downto 0); 
-  signal i_sel_code : std_logic_vector(1 downto 0); 
-  signal o_code     : std_logic_vector(1 downto 0); 
+  -- signal i_en       : std_logic;        
+  -- signal i_code_0   : std_logic_vector(1 downto 0); 
+  -- signal i_code_1   : std_logic_vector(1 downto 0); 
+  -- signal i_code_2   : std_logic_vector(1 downto 0); 
+  -- signal i_code_3   : std_logic_vector(1 downto 0); 
+  -- signal i_sel_code : std_logic_vector(1 downto 0); 
+  -- signal o_code     : std_logic_vector(1 downto 0); 
 
 
 
-  signal input : std_logic_vector (2 downto 0) := "000"; -- must be correct
+  signal input : std_logic_vector (4 downto 0) := "00000"; -- must be correct
 
 begin
   duv: entity work.micro(equation)
 
  
- port map( i_en       => i_en      ,             
-           i_code_0   => i_code_0  , 
-           i_code_1   => i_code_1  ,   
-           i_code_2   => i_code_2  ,   
-           i_code_3   => i_code_3  ,   
-           i_sel_code => i_sel_code, 
-           o_code     => o_code    
+  port map(i_en   => i_en  ,    
+           i_code => i_code,
+           o_A    => o_A   ,
+           o_L    => o_L   ,
+           o_B    => o_B     
           ); 
               
               
   apply_test_cases : process is
     procedure apply_test
-      ( itst : in std_logic_vector(2 downto 0)) is
-      -- ( e0, e1, e2, e3 : in std_logic) is                                  -- 4
-      -- ( e0, e1, e2, e3, e4, e5, e6, e7 : in std_logic) is                  -- 7
+      ( itst : in std_logic_vector(4 downto 0)) is
         
     begin 
         
-      i_sel_code <= itst(1 downto 0);
-      i_en       <= itst(2);
-      i_code_0   <= "00";
-      i_code_1   <= "01";
-      i_code_2   <= "10";
-      i_code_3   <= "11";
+      i_code <= itst(3 downto 0);
+      i_en       <= itst(4);
         
     
       wait for 1 ms;
@@ -59,14 +58,9 @@ begin
     
   begin
     for i in 0 to 10 loop
-      -- apply_test(input(3), input(2), input(1), input(0));
       apply_test(input);
-      
-
       input <= input + "0001";
-     end loop;
-
-    
+     end loop;    
     wait;
 end process apply_test_cases;
 
