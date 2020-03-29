deec353774d79f237138aa94e1def731a5e02a87
Brandon Valley <bavvh8@mst.edu>
2020-03-29 16:14:57 -0500

    8:3 works

--- verilog/priority_enc_8_3_tb_v.v
+++ verilog/priority_enc_8_3_tb_v.v
@@ -17,13 +17,28 @@ module priority_enc_8_3_tb_v;
   //procedure statement
   initial begin
 
-      for (i = 0 ; i < 258 ; i = i + 1) begin
-        #1000 i_code = d_in;
-        // #1000 o_code[0] = d_in[0]; o_code[1] = d_in[1]; o_code[2] = d_in[]; i_d = d_in[4];
-        d_in = d_in + 1;
-      end
+      // for (i = 0 ; i < 258 ; i = i + 1) begin
+        // #1000 i_code = d_in;
+        // // #1000 o_code[0] = d_in[0]; o_code[1] = d_in[1]; o_code[2] = d_in[]; i_d = d_in[4];
+        // d_in = d_in + 1;
+      // end
 
-      
+      #1000 i_code = 8'b00000000;
+      #1000 i_code = 8'b00000010;
+      #1000 i_code = 8'b00000100;
+      #1000 i_code = 8'b00001000;
+      #1000 i_code = 8'b00010000;
+      #1000 i_code = 8'b00100000;
+      #1000 i_code = 8'b01000000;
+      #1000 i_code = 8'b10000000;
+      #1000 i_code = 8'b10000000;
+      #1000 i_code = 8'b10000010;
+      #1000 i_code = 8'b10000100;
+      #1000 i_code = 8'b10001000;
+      #1000 i_code = 8'b10010000;
+      #1000 i_code = 8'b10100000;
+      #1000 i_code = 8'b11000000;
+      #1000 i_code = 8'b10000000;
       
       
           
