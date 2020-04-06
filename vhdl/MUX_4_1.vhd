171946a73f7dd39418ce6b3efee128760bac4a0d
Brandon Valley <bavvh8@mst.edu>
2020-04-05 21:07:27 -0500

    added high Z on disable for VVV

--- vhdl/MUX_4_1.vhd
+++ vhdl/MUX_4_1.vhd
@@ -15,36 +15,20 @@ end MUX_4_1;
 
 architecture equation of MUX_4_1 is
   begin
-
-  o_f <= '1' when  (i_sel_code = "00" and i_en = '1' and i_code(0) = '1') or 
-                   (i_sel_code = "01" and i_en = '1' and i_code(1) = '1') or 
-                   (i_sel_code = "10" and i_en = '1' and i_code(2) = '1') or 
-                   (i_sel_code = "11" and i_en = '1' and i_code(3) = '1') else '0';
+  
+  o_f <= 'Z' when  i_en = '0' else
+         '1' when  (i_sel_code = "00" and i_code(0) = '1') or 
+                   (i_sel_code = "01" and i_code(1) = '1') or 
+                   (i_sel_code = "10" and i_code(2) = '1') or 
+                   (i_sel_code = "11" and i_code(3) = '1') else '0';
+                   
+  
     
     
 end architecture equation;
 
 
     
-    
--- //  Behavior Model
--- module MUX_4_1_v
-  -- (
-  -- input        i_en,
-  -- input  [3:0] i_code,
-  -- input  [1:0] i_sel_code, 
-  -- output       o_f);
-  
-
-  -- assign o_f = i_sel_code == 2'b00 & i_code[0] & i_en ? 1 :
-               -- i_sel_code == 2'b01 & i_code[1] & i_en ? 1 :
-               -- i_sel_code == 2'b10 & i_code[2] & i_en ? 1 :
-               -- i_sel_code == 2'b11 & i_code[3] & i_en ? 1 : 0;
-  
--- endmodule
-
-
-
 
 
 
